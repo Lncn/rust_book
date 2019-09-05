383158d8828f87c21a16b3a16eebcfbf4827c6a3
Lincoln Simmons <lincolnsimmons@gmail.com>
2019-09-04 22:13:52 -0700

    Finishing 'minigrep' tool from Chapter 12

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,37 +1,18 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("Provided config: {:?}", config);
-
-    let contents = fs::read_to_string(config.filename).expect("Could not read the file!");
-
-    println!("Text:\n{}", contents);
-}
-
-#[derive(Debug)]
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Please provide valid number of args!");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
     }
 }
